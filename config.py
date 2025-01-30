00734693feb229f3c4ecd4bfa456c9e5682c66e1
Alex Beng <599267065@qq.com>
2025-01-31 01:11:43 +0800

    up framework

---
+++ config.py
@@ -0,0 +1,18 @@
+clock_config = {
+    # data relate
+    "data_files": [
+        {
+            "name": "ye_clock_1.json",
+            "session_id": 1,
+            "type": "clock"
+        },
+        {
+            "name": "jiang_clock_1.json",
+            "session_id": 1,
+            "type": "clock"
+        }
+    ],
+    
+    # train relate
+    
+}
--- train.py
+++ train.py
@@ -1,15 +1,47 @@
 from models import Policy
 import argparse
 
+def get_config(cfg, args, key):
+    if hasattr(args, key):
+        return getattr(args, key)
+    elif key in cfg:
+        return cfg[key] 
+
+def get_args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config-name", "--cn", required=True)
+    return parser
+
 class Workspace:
     def __init__(self, cfg, args):
-
+        # TODO: load data -> process to list of (scramble etc..)
         pass
-    # TODO: implement training loop
+    
+    # TODO: trainging loop
+    def train(self):
+        pass
+    
+    # TODO: infer scrambles
+    def infer(self):
+        pass
+
+    # TODO: trans to onnx model
+    def to_onnx(self):
+        pass
 
 if __name__ == "__main__":    
     #  using py file as config
-    import config as cfg
-    parser = argparse.ArgumentParser()
+    import config
 
-    workspace  = Workspace(cfg, parser.parse_args())
+    parser = get_args_parser()
+    args = parser.parse_args()
+    print(args)
+
+    config_name = args.config_name
+    if not hasattr(config, config_name):
+        print(f"[error] not config name in config.py: {config_name}")
+        exit(-1)
+    cfg = getattr(config, config_name)
+    print(cfg)
+
+    workspace  = Workspace(cfg, args)
